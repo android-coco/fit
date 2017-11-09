f8fef93c90060e6f951fabe8cf8f1dbc15350a75
youhao <yh_android@163.com>
2017-11-09 19:48:54 +0800

    model.go

--- model.go
+++ model.go
@@ -20,6 +20,11 @@ type JsonTime time.Time
 func (j JsonTime) MarshalJSON() ([]byte, error) {
 	return []byte(`"`+time.Time(j).Format("2006-01-02 15:04:05")+`"`), nil
 }
+
+func (j JsonTime) String() string {
+	return time.Time(j).Format("2006-01-02 15:04:05")
+}
+
 func MySqlEngine() *xorm.Engine  {
 	if engine == nil {
 		var err error
